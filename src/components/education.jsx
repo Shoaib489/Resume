c2302792cc0b93b7e569753f96ab1e72b5c42a95
Shoaib489 <SHOAIB22657@GMAIL.COM>
2024-08-09 15:29:55 +0530

    responsive

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -6,7 +6,6 @@ const Education = () => {
       <div className='p-[20px]'>
         <p className='text-[28px] font-semibold'>Education</p>
         <div className='text-[16px] text-slate-600 ml-1 p-2'>
-          
           <div className='flex flex-row gap-3 '>
             <p className='font-semibold text-[black]'>1.</p>
             <p >Completed 12th (in Non-Medical) from G.G.N. public school </p>
@@ -19,7 +18,6 @@ const Education = () => {
             <p className='font-semibold text-[black]'>3.</p>
             <p >I've been learning web development for the past year through a course at Staple Apps, gaining valuable skills and experience.". Ludhiana <span> (2023-24).</span></p>
           </div>
-         
         </div>
       </div>
       <hr />
---
+++ src/components/hobbies.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hobbies = () => {
+    return (
+        <div>
+            <p>hobbies</p>
+            <p>
+                
+            </p>
+        </div>
+    )
+}
+
+export default Hobbies
