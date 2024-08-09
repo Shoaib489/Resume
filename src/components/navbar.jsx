0633156c10d8ae1c9206b5d75fb9b24802176dcf
Shoaib489 <SHOAIB22657@GMAIL.COM>
2024-08-09 15:59:18 +0530

    change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,15 +7,16 @@ const Navbar = () => {
 
     return (
         <div className='max-w-[1280px]  m-auto  '>
-            
+
             <div className='flex p-[20px] justify-between items-center flex-wrap'>
                 <div className='flex-col'>
                     <p className={`${classes.nameHead} text-[40px] font-bold`}>Mohammad Shoaib</p>
                     <div className='flex-col gap-10'>
-                        <p className='flex gap-2 text-slate-600  items-center '>
-                            <IconPhoneCall stroke={1.5} className='text-blue-600 ' />
-                            <Link href={"tel-:77195-34499"}>77195-34499</Link>
+                        <p className='flex gap-2 text-slate-600 items-center'>
+                            <IconPhoneCall stroke={1.5} className='text-blue-600' />
+                            <Link href="tel:77195-34499">77195-34499</Link>
                         </p>
+
                         <p className='flex gap-2 text-slate-600 items-center  '>
                             <IconMail stroke={1.5} className='text-blue-600 ' />
                             <Link href="mailto:shoaib22657@gmail.com">shoaib22657@gmail.com</Link>
